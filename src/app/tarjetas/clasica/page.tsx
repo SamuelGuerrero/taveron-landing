aa38ea298e9b69baba7d9153cbb84ea671082c6b
SamuelGuerrero <itougn15@gmail.com>
2026-07-01 20:42:34 -0600

    💄 Render ClassicHero on the /tarjetas/clasica route

--- src/app/tarjetas/clasica/page.tsx
+++ src/app/tarjetas/clasica/page.tsx
@@ -1,11 +1,9 @@
-import { Text } from "~/components/atoms/Text/Text";
+import { ClassicHero } from "~/components/templates";
 
 export default function TarjetaClasicaPage() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <Text as="h1" weight="bold" size={48}>
-        Tarjeta Clásica
-      </Text>
+    <main>
+      <ClassicHero />
     </main>
   );
 }
